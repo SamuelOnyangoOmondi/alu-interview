13fc27d211cada9eaa8ce8308e44f41751a3f107
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-03-15 05:37:25 +0300

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-#include <stdio.h>
-int main() 
-{
-   int rows, coef = 1, space, i, j;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 0; i < rows; i++) 
-   {
-      for (space = 1; space <= rows - i; space++)
-         printf("  ");
-      for (j = 0; j <= i; j++) 
-      {
-         if (j == 0 || i == 0)
-            coef = 1;
-         else
-            coef = coef * (i - j + 1) / j;
-         printf("%4d", coef);
-      }
-      printf("\n");
-   }
-   return 0;
-}
+'''
+Returns list representing the Pascal's triangle of n
+'''
+
+
+def pascal_triangle(n):
+    '''returns empty list if n <= 0'''
+    if n <= 0:
+        return []
+
+    triangle = []
+    row = []
+    prev_row = []
+    for i in range(0, n + 1):
+        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] +
+               prev_row[j] or 1 for j in range(0, i)]
+        prev_row = row
+        triangle += [row]
+    return triangle[1:]
